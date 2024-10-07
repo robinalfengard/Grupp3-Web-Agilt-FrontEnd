67d98081259795b4fb53e521803feb107cd8d8d8
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-10-07 11:07:19 +0200

    Create Landing Page

---
+++ src/components/Landning.vue
@@ -0,0 +1,13 @@
+<template>
+  <h1>Landning sidan</h1>
+  <p>Detta är min landningssida</p>
+</template>
+
+<script>
+export default {
+  name: 'LandningPage',
+};
+</script>
+
+<style scoped>
+</style>
