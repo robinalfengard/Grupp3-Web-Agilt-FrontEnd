48c96c0b167b3a29e63d9e3bff15dbdb2b97408c
Robin Alfengård <robin.alfengard@hotmail.com>
2024-10-07 10:44:04 +0200

    started with bootrstap

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <SignupComponent/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SignupComponent from './components/SignupComponent.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    SignupComponent
   }
 }
 </script>
--- src/components/LandingPage.vue
+++ src/components/LandingPage.vue
@@ -32,7 +32,7 @@
 
 <script>
 export default {
-  name: 'HelloWorld',
+  name: 'LandingPage',
   props: {
     msg: String
   }
