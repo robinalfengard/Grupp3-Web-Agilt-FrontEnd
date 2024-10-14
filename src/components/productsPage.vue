911e7dea147d4d88b41205b04c671e10fe787ee5
maxerkmar <max.erkmar@iths.se>
2024-10-14 14:15:45 +0200

    Added function to sort items on price

--- src/components/productsPage.vue
+++ src/components/productsPage.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="container">
+    <div class="d-flex justify-content-end mb-4 mt-2">
+      <div class="dropdown">
+        <button
+          class="btn btn-light dropdown-toggle fw-bold"
+          type="button"
+          id="sortByDropdown"
+          data-bs-toggle="dropdown"
+          aria-expanded="false"
+        >
+          Sort By <i class="bi bi-chevron-down"></i>
+        </button>
+        <ul class="dropdown-menu" aria-labelledby="sortByDropdown">
+          <li><a class="dropdown-item" @click.prevent="changeSort('lowest')" href="#">Lowest Price First</a></li>
+          <li><a class="dropdown-item" @click.prevent="changeSort('highest')" href="#">Highest Price First</a></li>
+        </ul>
+      </div>
+    </div>
+    
     <p v-if="loading" class="text-center">Loading products...</p>
     <p v-if="error" class="text-center text-danger">{{ error }}</p>
     <div v-if="products.length && !loading" class="row">
@@ -9,7 +27,6 @@
           <div class="card-body">
             <h2 class="card-title text-center">{{ product.name }}</h2>
             <h5 v-if="product.onSale" class="text-danger text-center">ON SALE</h5>
-            <h5 v-if="!product.onSale" class="text-danger text-center"><br></h5>
             <p class="text-center">{{ product.price }} kr</p>
             <p class="card-text">{{ product.description }}</p>
             <button @click="addToCart(product)" class="btn btn-primary w-100">Add to cart</button>
@@ -22,12 +39,14 @@
 </template>
 
 <script setup>
-import {ref, onMounted} from 'vue';
-import {useRoute} from 'vue-router';
+import { ref, onMounted } from 'vue';
+import { useRoute } from 'vue-router';
 
 const products = ref([]);
 const route = useRoute();
 const categoryId = route.params.id;
+const loading = ref(true);
+
 
 const fetchProducts = async () => {
   const response = await fetch(`http://localhost:8080/product/category/${categoryId}`);
@@ -36,6 +55,15 @@ const fetchProducts = async () => {
   }
   const data = await response.json();
   products.value = data;
+  loading.value = false;
+};
+
+const changeSort = (criteria) => {
+  if (criteria === 'lowest') {
+    products.value.sort((a, b) => a.price - b.price);
+  } else {
+    products.value.sort((a, b) => b.price - a.price);
+  }
 };
 
 onMounted(() => {
@@ -48,11 +76,9 @@ onMounted(() => {
   height: auto;
   object-fit: cover;
 }
-
 .card {
   transition: transform 0.3s;
 }
-
 .card:hover {
   transform: scale(1.05);
 }
