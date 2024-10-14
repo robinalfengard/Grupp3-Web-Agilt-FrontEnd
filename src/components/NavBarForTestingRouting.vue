bb1c241af1a583463cc6b00eea1afce7124ae916
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-10-14 14:36:24 +0200

    Add /cart to NavBarForTestingRouting.vue

--- src/components/NavBarForTestingRouting.vue
+++ src/components/NavBarForTestingRouting.vue
@@ -1,22 +1,28 @@
 <template>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid"> 
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
       <a class="navbar-brand" href="#">Clothing Store</a>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-            <router-link to="/">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" h>Home</a>
-          </li>
-        </router-link>
-        <router-link to="/login">
-          <li class="nav-item">
-            <a class="nav-link">Login</a>
-        </li>
-    </router-link>
+          <router-link to="/">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" >Home</a>
+            </li>
+          </router-link>
+          <router-link to="/cart">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page">Shopping Cart</a>
+            </li>
+          </router-link>
+          <router-link to="/login">
+            <li class="nav-item">
+              <a class="nav-link">Login</a>
+            </li>
+          </router-link>
         </ul>
       </div>
     </div>
@@ -25,7 +31,7 @@
 
 <script>
 export default {
-    name: 'NavBarForTestingRouting',
+  name: 'NavBarForTestingRouting',
 
-  }
+}
 </script>
