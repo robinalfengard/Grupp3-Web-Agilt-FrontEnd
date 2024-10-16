22c7d699e8f694f6367389e12935fe565699b048
Robin Alfengård <robin.alfengard@hotmail.com>
2024-10-16 12:18:42 +0200

    Fixed loggedInStatus

--- src/components/NavBarForTestingRouting.vue
+++ src/components/NavBarForTestingRouting.vue
@@ -1,24 +1,25 @@
 <template>
-      <nav class="navbar navbar-expand-lg navbar-light bg-light">
-    <div class="container-fluid"> 
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
       <router-link to="/">
-      <a class="navbar-brand" href="#">Clothing Store</a>
+        <a class="navbar-brand" href="#">Clothing Store</a>
       </router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+        aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
-            <router-link to="/mypage">
-          <li class="nav-item">
-            <a class="nav-link active" aria-current="page" h>MyPage</a>
-          </li>
-        </router-link>
-        <router-link to="/login">
-          <li class="nav-item">
-            <a class="nav-link">Login</a>
-        </li>
-    </router-link>
+          <router-link to="/mypage">
+            <li class="nav-item" v-if="isLoggedIn">
+              <a class="nav-link active" aria-current="page" h>My Page</a>
+            </li>
+          </router-link>
+          <router-link to="/login">
+            <li class="nav-item">
+              <a @click="logout()" class="nav-link" v-text="isLoggedIn ? 'Logout' : 'Login'"></a>
+            </li>
+          </router-link>
         </ul>
       </div>
     </div>
@@ -27,7 +28,34 @@
 
 <script>
 export default {
-    name: 'NavBarForTestingRouting',
-
+  name: 'NavBarForTestingRouting',
+  data() {
+    return {
+      isLoggedIn: false
+    }
+  },
+  
+  mounted() {
+    this.checkLogin(); 
+  },
+  watch: {
+    $route() {
+      this.checkLogin();
+    }
+  },
+  methods: {
+    checkLogin() {
+      const token = localStorage.getItem('user');
+      if (token) {
+        this.isLoggedIn = true;
+      } else {
+        this.isLoggedIn = false;
+      }
+    },
+    logout() {
+      localStorage.removeItem('user');
+      this.isLoggedIn = false;
+    }
   }
+}
 </script>
