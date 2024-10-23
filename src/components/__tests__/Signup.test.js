4ca987bc58d853d7e4c2cf74e0cc6dedae7d2e62
Robin Alfengård <robin.alfengard@hotmail.com>
2024-10-23 13:35:02 +0200

    added test for login + config to run test + start on npm start

--- src/components/__tests__/Signup.test.js
+++ src/components/__tests__/Signup.test.js
@@ -1,7 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import { mount } from '@vue/test-utils';
 import Signup from '../Signup.vue';
-import axios from 'axios';
 
 describe('Signup', () => {
   it('Unfilled form return false', () => {
