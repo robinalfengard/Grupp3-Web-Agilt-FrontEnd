4c3b511c8f15c76a8f525c3d0efdc56b8a167e8f
Robin Alfengård <robin.alfengard@hotmail.com>
2024-10-23 12:04:48 +0200

    added a new test for signup

--- src/components/__tests__/Signup.test.js
+++ src/components/__tests__/Signup.test.js
@@ -1,6 +1,7 @@
 import { describe, it, expect } from 'vitest';
-import { createApp, mount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import Signup from '../Signup.vue';
+import axios from 'axios';
 
 describe('Signup', () => {
   it('Unfilled form return false', () => {
@@ -21,6 +22,28 @@ describe('Signup', () => {
     wrapper.find('input#city').setValue(city);
 
     expect(wrapper.vm.validateForm()).toBe(false);
+  });
+
+  it('Infotext should inform which field is unfilled', () => {
+    const firstName = 'Robin';
+    const lastName = 'Alfen'; 
+    const email = 'hej@hj.com'; 
+    const password = 'hej123'; 
+    const address = 'Gata 23';
+    const city = '';
+    const infoText = '';
+
+    const wrapper = mount(Signup);
+
+    wrapper.find('input#firstname').setValue(firstName);
+    wrapper.find('input#lastname').setValue(lastName);
+    wrapper.find('input#email').setValue(email);
+    wrapper.find('input#password').setValue(password);
+    wrapper.find('input#address').setValue(address);
+    wrapper.find('input#city').setValue(city);
+    wrapper.vm.validateForm();
+
+    expect(wrapper.vm.infoText.valueOf()).toBe('Please enter your city');
   });
 
   it('Filled form return true', () => {
