44b3e63f2b91dc3bf3a61e990eac18153bb408d8
Kourosh Talebzadeh <Kourosh.Talebzadeh@iths.se>
2024-10-29 18:32:18 +0100

    Add icon to NavBarForTestingRouting.vue

--- src/components/NavBarForTestingRouting.vue
+++ src/components/NavBarForTestingRouting.vue
@@ -1,21 +1,41 @@
 <template>
+  <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css"
+  />
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
     <div class="container-fluid">
-      <router-link to="/" class="navbar-brand">Clothing Store</router-link>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
-              aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <router-link to="/" class="navbar-brand">
+        <i class="bi bi-house custom-icon"></i>
+      </router-link>
+      <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+      >
         <span class="navbar-toggler-icon"></span>
       </button>
       <div class="collapse navbar-collapse" id="navbarNav">
         <ul class="navbar-nav ms-auto">
           <li class="nav-item" v-if="isLoggedIn">
-            <router-link to="/mypage" class="nav-link active" aria-current="page">My Page</router-link>
+            <router-link to="/mypage" class="nav-link active" aria-current="page">
+              <i class="bi bi-person-circle custom-icon"></i>
+            </router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/login" class="nav-link" @click="logout()">{{ isLoggedIn ? 'Logout' : 'Login' }}</router-link>
+            <router-link to="/login" class="nav-link" @click="logout">
+              <i v-if="isLoggedIn" class="bi bi-box-arrow-right custom-icon"></i>
+              <i v-else class="bi bi-person custom-icon"></i>
+            </router-link>
           </li>
           <li class="nav-item">
-            <router-link to="/cart" class="nav-link active" aria-current="page">Shopping Cart</router-link>
+            <router-link to="/cart" class="nav-link active" aria-current="page">
+              <i class="bi bi-cart4 custom-icon"></i>
+            </router-link>
           </li>
         </ul>
       </div>
@@ -25,11 +45,11 @@
 
 <script>
 export default {
-  name: 'NavBarForTestingRouting',
+  name: "NavBarForTestingRouting",
   data() {
     return {
-      isLoggedIn: false
-    }
+      isLoggedIn: false,
+    };
   },
   mounted() {
     this.checkLogin();
@@ -37,17 +57,30 @@ export default {
   watch: {
     $route() {
       this.checkLogin();
-    }
+    },
   },
   methods: {
     checkLogin() {
-      const token = localStorage.getItem('user');
+      const token = localStorage.getItem("user");
       this.isLoggedIn = !!token; // true if token exists
     },
     logout() {
-      localStorage.removeItem('user');
+      localStorage.removeItem("user");
       this.isLoggedIn = false;
-    }
-  }
-}
+    },
+  },
+};
 </script>
+
+<style scoped>
+/* Styling for icons */
+.custom-icon {
+  font-size: 2rem;
+  color: #007bff;
+}
+
+/* Additional hover effect */
+.nav-link:hover .custom-icon {
+  color: #d22ead; /* Darker color on hover */
+}
+</style>
